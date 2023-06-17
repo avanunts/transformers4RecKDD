a19573a29f7daa5f16c198aa46c96a9e4957e115
Arsenii Vanunts <ar16sen@gmail.com>
2023-06-17 11:32:52 +0200

    Revert "Experimenting with nvt"
    This reverts commit ec1da4fdd05538608e1e216e1a28ee3fda24d3bf.

--- nvt_workflows.py
+++ nvt_workflows.py
@@ -6,7 +6,7 @@ from merlin.schema import Tags
 SESSIONS_MAX_LENGTH = 20
 
 
-def train_workflow_v1():
+def workflow_v1():
     cat_feats = ColumnSelector(['item']) >> nvt.ops.Categorify()
     features = ColumnSelector(['session_id', 'time']) + cat_feats
 
@@ -23,10 +23,3 @@ def train_workflow_v1():
     groupby_features_truncated = groupby_features_list >> nvt.ops.ListSlice(-SESSIONS_MAX_LENGTH)
 
     return nvt.Workflow(groupby_features_truncated)
-
-
-def eval_workflow_v1():
-    item_list = ColumnSelector(['item-list'])
-    features = item_list >> nvt.ops.ListSlice(0, end=-1)
-    last_item = item_list >> nvt.ops.ListSlice(-1) >> nvt.ops.Rename(name='last-item')
-    return nvt.Workflow(features + last_item)
