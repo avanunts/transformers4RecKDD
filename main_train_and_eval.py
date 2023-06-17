88b09c29feb7c81118a4d838ec17626ca990279c
Arsenii Vanunts <ar16sen@gmail.com>
2023-06-17 16:39:23 +0200

    Added resume_from_checkpoint argument to end-to-end train and eval

--- main_train_and_eval.py
+++ main_train_and_eval.py
@@ -10,12 +10,13 @@ args:
 - nvt_paths dict of form {'train': nvt_train_path, 'test': nvt_test_path}
 - xlnet_args (see t4rec_models module)
 - custom_training_non_fixed_args (see t4rec_trainers module)
+- resume_from_checkpoint (if train was already launched but interrupted for some reason)
 
 returns trainer object, in which model, train, test and eval results might be found
 '''
 
 
-def train_and_eval_xlnet(nvt_paths, xlnet_args, custom_training_non_fixed_args):
+def train_and_eval_xlnet(nvt_paths, xlnet_args, custom_training_non_fixed_args, resume_from_checkpoint):
     train = Dataset(nvt_paths['train'], engine='parquet')
     test = Dataset(nvt_paths['test'], engine='parquet')
     model = t4rec_models.xl_net_model(xlnet_args)
@@ -28,7 +29,7 @@ def train_and_eval_xlnet(nvt_paths, xlnet_args, custom_training_non_fixed_args):
     )
     trainer.train_dataset_or_path = train
     trainer.eval_dataset_or_path = test
-    trainer.train(resume_from_checkpoint=False)
+    trainer.train(resume_from_checkpoint=resume_from_checkpoint)
     return trainer
 
 
