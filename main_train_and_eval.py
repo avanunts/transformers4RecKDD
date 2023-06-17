8f731ad7829147a34051a592573c4963575baa41
Arsenii Vanunts <ar16sen@gmail.com>
2023-06-17 16:56:03 +0200

    Throwed schema inference to end-to-end

--- main_train_and_eval.py
+++ main_train_and_eval.py
@@ -8,7 +8,7 @@ from transformers4rec.torch import Trainer
 '''
 args:
 - nvt_paths dict of form {'train': nvt_train_path, 'test': nvt_test_path}
-- xlnet_args (see t4rec_models module)
+- xlnet_args (see t4rec_models module, here schema is overriden, so you don't have to assign it there)
 - custom_training_non_fixed_args (see t4rec_trainers module)
 - resume_from_checkpoint (if train was already launched but interrupted for some reason)
 
@@ -19,6 +19,7 @@ returns trainer object, in which model, train, test and eval results might be fo
 def train_and_eval_xlnet(nvt_paths, xlnet_args, custom_training_non_fixed_args, resume_from_checkpoint):
     train = Dataset(nvt_paths['train'], engine='parquet')
     test = Dataset(nvt_paths['test'], engine='parquet')
+    xlnet_args['schema'] = train.schema
     model = t4rec_models.xl_net_model(xlnet_args)
     training_args = t4rec_trainers.CustomTrainingArguments(custom_training_non_fixed_args)
     trainer = Trainer(
