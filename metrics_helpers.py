74ca3a6636c55112d302e814d7236d6ad657e9a2
Arsenii Vanunts <ar16sen@gmail.com>
2023-06-19 12:49:07 +0200

    Finalized metrics_helpers

--- metrics_helpers.py
+++ metrics_helpers.py
@@ -1,5 +1,6 @@
-import log_history_helpers
+import pandas as pd
 
+import log_history_helpers
 
 RESULTS_KV = {
     'train_num_epochs': log_history_helpers.get_train_num_epochs,
@@ -13,6 +14,15 @@ RESULTS_KV = {
     'mrr@40_last_cp': lambda x: log_history_helpers.get_mrr_last(x, 40),
 }
 
+RESULTS_COLUMNS = ['model_name'] + list(RESULTS_KV.keys())
+
+
+def save_metrics(trainer, model_name, results_file_path):
+    results_entry = get_metrics(trainer)
+    results_entry['model_name'] = model_name
+    pd.DataFrame([results_entry], columns=RESULTS_COLUMNS).to_csv(results_file_path, mode='a', index=False,
+                                                                  header=False)
+
 
 def get_metrics(trainer):
     log_history = trainer.state.log_history
@@ -20,3 +30,7 @@ def get_metrics(trainer):
         lambda item: (item[0], item[1](log_history)),
         RESULTS_KV.items()
     ))
+
+
+def init_results_file(results_file_path):
+    pd.DataFrame([], columns=RESULTS_COLUMNS).to_csv(results_file_path, index=False, header=True)
