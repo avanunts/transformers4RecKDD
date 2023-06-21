ca0977dc813a3cb472d6a9685599a4ff76c13ac0
Arsenii Vanunts <ar16sen@gmail.com>
2023-06-21 19:39:37 +0200

    Fix

--- losses.py
+++ losses.py
@@ -9,7 +9,7 @@ class BPRMaxLoss(nn.Module):
     def forward(self, input, target):
         m1 = nn.Softmax(dim=1)
         m2 = nn.Sigmoid()
-        ri = torch.gather(input, 1, target)
+        ri = torch.gather(input, 1, target[:, None])
         sigmoids = m2(ri - input)
         expits = m1(input)
         scores_exp = torch.sum(expits * sigmoids, dim=1)
