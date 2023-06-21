7525bf3838ca19e466326aa7a3de4df92f16a59d
Arsenii Vanunts <ar16sen@gmail.com>
2023-06-21 14:54:17 +0200

    Fixed hdd_utils

--- hdd_utils.py
+++ hdd_utils.py
@@ -1,18 +1,16 @@
 import os
+import shutil
 
 
-def clear_checkpoints(dir_path, names_to_leave, dry_run=True):
-    all_dirs_and_files = os.listdir(dir_path)
-    rm_dirs_and_files = sorted(list(filter(lambda x: x not in names_to_leave, all_dirs_and_files)))
+def clear_checkpoints(folder_path, names_to_leave, dry_run=True):
+    all_dir_and_file_names = os.listdir(folder_path)
+    is_dir_name_to_remove = lambda x: x not in names_to_leave and os.path.isdir(os.path.join(folder_path, x))
+    rm_dirs = sorted(list(filter(is_dir_name_to_remove, all_dir_and_file_names)))
     if dry_run:
         print('DRY RUN: files and dirs will not be removed, to remove them set dry_run=False')
-        for dir_or_file_name in rm_dirs_and_files:
-            print(dir_or_file_name)
+        for dir_name in rm_dirs:
+            print(dir_name)
     else:
-        for dir_or_file_name in rm_dirs_and_files:
-            path = os.path.join(dir_path, dir_or_file_name)
-            if os.path.isfile(path):
-                os.remove(path)
-            elif os.path.isdir(path):
-                os.rmdir(path)
-
+        for dir_name in rm_dirs:
+            dir_path = os.path.join(folder_path, dir_name)
+            shutil.rmtree(dir_path, ignore_errors=True)
