0207e66a7d24a376ba38924ca5620fe122a321d9
Arsenii Vanunts <ar16sen@gmail.com>
2023-06-23 18:24:23 +0200

    Added workflow with price and brand

--- nvt_workflows.py
+++ nvt_workflows.py
@@ -1,7 +1,5 @@
-import numpy as np
 import nvtabular as nvt
 from merlin.dag import ColumnSelector
-from merlin.schema import Tags
 
 SESSIONS_MAX_LENGTH = 20
 
@@ -20,6 +18,29 @@ def workflow_v1():
 
     item_feat = groupby_features['item-list'] >> nvt.ops.TagAsItemID()
     groupby_features_list = item_feat
+    groupby_features_truncated = groupby_features_list >> nvt.ops.ListSlice(-SESSIONS_MAX_LENGTH)
+
+    return nvt.Workflow(groupby_features_truncated)
+
+
+def workflow_v2():
+    price_feat = ColumnSelector(['price']) >> (lambda x: x + 1) >> nvt.ops.Clip(min_value=1, max_value=10000) \
+                 >> nvt.ops.LogOp() >> (lambda x: x.astype("int32"))
+    cat_feats = ColumnSelector(['item', 'brand']) + price_feat >> nvt.ops.Categorify()
+
+    features = ColumnSelector(['session_id', 'time']) + cat_feats
+    groupby_features = features >> nvt.ops.Groupby(
+        groupby_cols=["session_id"],
+        sort_cols=["time"],
+        aggs={
+            'item': ["list"],
+            'brand': ['list'],
+            'price': ['list'],
+        },
+        name_sep="-")
+    item_feat = groupby_features['item-list'] >> nvt.ops.TagAsItemID()
+
+    groupby_features_list = item_feat + groupby_features['brand-list', 'price-list']
     groupby_features_truncated = groupby_features_list >> nvt.ops.ListSlice(-SESSIONS_MAX_LENGTH)
 
     return nvt.Workflow(groupby_features_truncated)
