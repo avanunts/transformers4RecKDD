1cbc4279bb75e4836b6f7550543f9a42eadc7324
Arsenii Vanunts <ar16sen@gmail.com>
2023-06-27 12:34:43 +0200

    Fix

--- t4rec_modules.py
+++ t4rec_modules.py
@@ -3,9 +3,9 @@ from torch.nn import LayerNorm
 
 class T4RecLayerNorm(LayerNorm):
     def __init__(self, input_shape):
-        self.output_size = input_shape
+        self.output_size_var = input_shape
         super().__init__(normalized_shape=input_shape[-1])
 
     def output_size(self):
-        return self.output_size
+        return self.output_size_var
 
