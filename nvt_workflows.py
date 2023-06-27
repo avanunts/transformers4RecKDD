c04e39b20453930b88a362b8392fd471c31faae5
Arsenii Vanunts <ar16sen@gmail.com>
2023-06-27 14:54:26 +0200

    Added workflows for sohe and straightforward price

--- nvt_workflows.py
+++ nvt_workflows.py
@@ -23,6 +23,9 @@ def workflow_v1():
     return nvt.Workflow(groupby_features_truncated)
 
 
+'''
+v2 - with brand and price, price -> log -> int -> embedding
+'''
 def workflow_v2():
     price_feat = ColumnSelector(['price']) >> nvt.ops.LambdaOp(lambda x: x + 1) >> \
                  nvt.ops.Clip(min_value=1, max_value=10000) >> nvt.ops.LogOp() >> \
@@ -30,6 +33,54 @@ def workflow_v2():
     cat_feats = ColumnSelector(['item', 'brand']) + price_feat >> nvt.ops.Categorify()
 
     features = ColumnSelector(['session_id', 'time']) + cat_feats
+    groupby_features = features >> nvt.ops.Groupby(
+        groupby_cols=["session_id"],
+        sort_cols=["time"],
+        aggs={
+            'item': ["list"],
+            'brand': ['list'],
+            'price': ['list'],
+        },
+        name_sep="-")
+    item_feat = groupby_features['item-list'] >> nvt.ops.TagAsItemID()
+
+    groupby_features_list = item_feat + groupby_features['brand-list', 'price-list']
+    groupby_features_truncated = groupby_features_list >> nvt.ops.ListSlice(-SESSIONS_MAX_LENGTH)
+
+    return nvt.Workflow(groupby_features_truncated)
+
+
+'''
+v3 - price as soft one-hot encoding
+'''
+def workflow_v3():
+    cat_feats = ColumnSelector(['item', 'brand']) >> nvt.ops.Categorify()
+    features = ColumnSelector(['session_id', 'time', 'price']) + cat_feats
+    groupby_features = features >> nvt.ops.Groupby(
+        groupby_cols=["session_id"],
+        sort_cols=["time"],
+        aggs={
+            'item': ["list"],
+            'brand': ['list'],
+            'price': ['list'],
+        },
+        name_sep="-")
+    item_feat = groupby_features['item-list'] >> nvt.ops.TagAsItemID()
+
+    groupby_features_list = item_feat + groupby_features['brand-list', 'price-list']
+    groupby_features_truncated = groupby_features_list >> nvt.ops.ListSlice(-SESSIONS_MAX_LENGTH)
+
+    return nvt.Workflow(groupby_features_truncated)
+
+
+'''
+v4 - price as numerical feature
+'''
+def workflow_v4():
+    price_feat = ColumnSelector(['price']) >> nvt.ops.Normalize()
+    cat_feats = ColumnSelector(['item', 'brand']) >> nvt.ops.Categorify()
+    features = ColumnSelector(['session_id', 'time']) + cat_feats + price_feat
+
     groupby_features = features >> nvt.ops.Groupby(
         groupby_cols=["session_id"],
         sort_cols=["time"],
