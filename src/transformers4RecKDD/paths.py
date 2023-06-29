dac34c531e5a573a7e524e45d0a3a0fa3a0c51eb
Arsenii Vanunts <ar16sen@gmail.com>
2023-06-29 13:33:29 +0200

    Change

--- src/transformers4RecKDD/paths.py
+++ src/transformers4RecKDD/paths.py
@@ -1,33 +1,39 @@
 import os
 import json
-from importlib_resources import files
 
-folders = files('paths').joinpath('folders').read_text(encoding="utf-8")
-entry_points = json.load(folders)
-KDD_DATA_FOLDER = entry_points['kdd_data']
-T4REC_DATA_FOLDER = entry_points['t4rec_data']
-T4REC_MODELS_FOLDER = entry_points['t4rec_models']
-
-
-def kdd_sessions_path(locale, typ):
-    return os.path.join(KDD_DATA_FOLDER, '{}_sessions_{}.parquet'.format(locale, typ))
+'''
+typ: dev_train, dev_test, submit_train, submit_predict
+locale: DE, JP, UP, IT, ES, FR
+paths_config_path: path to config json, in which kdd_data_folder, t4rec_data_folder, t4rec_models_folder paths are specified
+version: int 
+'''
 
 
-def kdd_products_path(locale):
-    return os.path.join(KDD_DATA_FOLDER, '{}_products.parquet'.format(locale))
+def kdd_sessions_path(locale, typ, paths_config_path):
+    config = json.loads(paths_config_path)
+    return os.path.join(config['kdd_data_folder'], '{}_sessions_{}.parquet'.format(locale, typ))
 
 
-def t4rec_cu_ds_path(locale, typ, version: int):
-    return os.path.join(T4REC_DATA_FOLDER, '{}_{}_v{}_processed.parquet'.format(locale, typ, version))
+def kdd_products_path(locale, paths_config_path):
+    config = json.loads(paths_config_path)
+    return os.path.join(config['kdd_data_folder'], '{}_products.parquet'.format(locale))
 
 
-def t4rec_nvt_ds_path(locale, typ, version: int):
-    return os.path.join(T4REC_DATA_FOLDER, '{}_{}_v{}_processed_nvt'.format(locale, typ, version))
+def t4rec_cu_ds_path(locale, typ, version: int, paths_config_path):
+    config = json.loads(paths_config_path)
+    return os.path.join(config['t4rec_data_folder'], '{}_{}_v{}_processed.parquet'.format(locale, typ, version))
 
 
-def t4rec_model_path(locale, typ, version):
-    return os.path.join(T4REC_MODELS_FOLDER, '{}_{}_v{}'.format(locale, typ, version))
+def t4rec_nvt_ds_path(locale, typ, version: int, paths_config_path):
+    config = json.loads(paths_config_path)
+    return os.path.join(config['t4rec_data_folder'], '{}_{}_v{}_processed_nvt'.format(locale, typ, version))
 
 
-def nvt_workflow_path(locale, version: int):
-    return os.path.join(T4REC_DATA_FOLDER, "{}_v{}_workflow_etl".format(locale, version))
+def t4rec_model_path(locale, typ, version, paths_config_path):
+    config = json.loads(paths_config_path)
+    return os.path.join(config['t4rec_models_folder'], '{}_{}_v{}'.format(locale, typ, version))
+
+
+def nvt_workflow_path(locale, version: int, paths_config_path):
+    config = json.loads(paths_config_path)
+    return os.path.join(config['t4rec_data_folder'], "{}_v{}_workflow_etl".format(locale, version))
