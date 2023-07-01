ac1c2d36ef28e295fef9ad79743d1c7b67f6982e
Arsenii Vanunts <ar16sen@gmail.com>
2023-07-01 11:31:24 +0200

    Added train many

--- src/transformers4RecKDD/main/train.py
+++ src/transformers4RecKDD/main/train.py
@@ -1,5 +1,6 @@
 import os
 import json
+import time
 
 from merlin.io import Dataset
 from ..t4rec.training_args import CustomTrainingArguments
@@ -15,7 +16,24 @@ MODEL_CONFIGS = {
 }
 
 
-def train(config_path):
+def train_many(*config_paths):
+    for config_path in config_paths:
+        if not check_config_path_to_model_name_bijection(config_path):
+            print('Skipping this train, because check is not successful')
+        t1 = time.time()
+        try:
+            train_one(config_path)
+        except Exception as err:
+            print('Exception for config at {} has occurred. Continue without saving time. '
+                  'Exception: {}'.format(config_path, err))
+            continue
+        t2 = time.time()
+        print('Total training time for config at {} is {:.2f}s'.format(config_path, t2 - t1))
+        save_additional_info(config_path, t1, t2)
+
+
+# do not use this method directly; if you want to train one model use train_many with a length one list arg
+def train_one(config_path):
     with open(config_path, 'r') as open_file:
         config = json.load(open_file)
 
@@ -57,16 +75,15 @@ def checkpoint_exists(dir_path):
 
 
 def get_paths_from_config(config):
-    t4rec_data_folder_path = config['t4rec_data_folder_path']
+    t4rec_folder_path = config['t4rec_folder_path']
     locale = config['locale']
     env = config['env']
     cu_version = config['cu_version']
     workflow_version = config['workflow_version']
-    train_path = t4rec_nvt_ds_path(t4rec_data_folder_path, locale, env, 'train', cu_version, workflow_version)
-    test_path = t4rec_nvt_ds_path(t4rec_data_folder_path, locale, env, 'test', cu_version, workflow_version)
+    train_path = t4rec_nvt_ds_path(t4rec_folder_path, locale, env, 'train', cu_version, workflow_version)
+    test_path = t4rec_nvt_ds_path(t4rec_folder_path, locale, env, 'test', cu_version, workflow_version)
 
-    t4rec_models_folder_path = config['t4rec_models_folder_path']
-    model_output_dir_path = t4rec_model_path(t4rec_models_folder_path, locale, env, config['model_name'])
+    model_output_dir_path = t4rec_model_path(t4rec_folder_path, locale, env, config['model_name'])
     create_folder_for_path_if_not_exists(os.path.dirname(model_output_dir_path))
     return train_path, test_path, model_output_dir_path
 
@@ -81,3 +98,47 @@ def init_model(model_type, model_config, schema):
     if model_type not in MODEL_CONSTRUCTORS or model_type not in MODEL_CONFIGS:
         raise ValueError('There is no defined constructor for model type {}'.format(model_type))
     return MODEL_CONSTRUCTORS[model_type](model_config, schema)
+
+
+def save_additional_info(config_path, t1, t2):
+    with open(config_path, 'r') as open_file:
+        config = json.load(open_file)
+    _, _, model_output_dir_path = get_paths_from_config(config)
+    add_info_path = os.path.join(model_output_dir_path, 'add_info.json')
+    if os.path.exists(add_info_path):
+        return
+    add_info = {
+        'config_path': config_path,
+        'first_iter_num_epochs': config['training_args']['num_train_epochs'],
+        'time_start': t1,
+        'time_end': t2,
+        'total_time_seconds': t2 - t1,
+        'time_per_epoch': (t2 - t1) / config['training_args']['num_train_epochs']
+    }
+
+    json_object = json.dumps(add_info, indent=4)
+    with open(add_info_path, "w") as outfile:
+        outfile.write(json_object)
+
+
+def check_config_path_to_model_name_bijection(config_path):
+    print('Start check for config_path <-> model_name bijection')
+    with open(config_path, 'r') as open_file:
+        config = json.load(open_file)
+    _, _, model_output_dir_path = get_paths_from_config(config)
+    add_info_path = os.path.join(model_output_dir_path, 'add_info.json')
+    if not os.path.exists(add_info_path):
+        print('Model output dir at path {} exists, '
+              'but add_info at path {} doesn\'t, check failed.'.format(model_output_dir_path, add_info_path))
+        return False
+    with open(add_info_path, 'r') as open_file:
+        add_info = json.load(open_file)
+    if add_info['config_path'] != config_path:
+        print('This model name already reserved for the config at path {}, '
+              'trying to use it to train model with config'
+              ' at path {}, check failed.'.format(add_info['config_path'], config_path))
+        return False
+    print('Check succeeded.')
+    return True
+
+
--- src/transformers4RecKDD/paths.py
+++ src/transformers4RecKDD/paths.py
@@ -8,40 +8,40 @@ typ: train/test
 cu_version: 1, 2, 3, ... - version of cu_ds for nvt_dataset
 workflow_version: 1, 2, 3, ... - version of nvt workflow
  
-paths_config_path: path to config json, in which kdd_data_folder, t4rec_data_folder, t4rec_models_folder paths are specified
+paths_config_path: path to config json, in which t4rec_folder_path is specified
 '''
 
 
-def kdd_sessions_path(kdd_data_folder_path, locale, env, typ):
-    return os.path.join(kdd_data_folder_path, locale, env, '{}_sessions.parquet'.format(typ))
+def kdd_sessions_path(t4rec_folder_path, locale, env, typ):
+    return os.path.join(t4rec_folder_path, 'data', 'kdd_datasets', locale, env, '{}_sessions.parquet'.format(typ))
 
 
-def kdd_products_path(kdd_data_folder_path, locale):
-    return os.path.join(kdd_data_folder_path, locale, 'products.parquet')
+def kdd_products_path(t4rec_folder_path, locale):
+    return os.path.join(t4rec_folder_path, 'data', 'kdd_datasets', locale, 'products.parquet')
 
 
-def t4rec_cu_ds_path(t4rec_data_folder_path, locale, env, typ, cu_version: int):
+def t4rec_cu_ds_path(t4rec_folder_path, locale, env, typ, cu_version: int):
     return os.path.join(
-        t4rec_data_folder_path, 'cu_datasets', locale, env,  '{}_v{}.parquet'.format(typ, cu_version)
+        t4rec_folder_path, 'data', 'cu_datasets', locale, env,  '{}_v{}.parquet'.format(typ, cu_version)
     )
 
 
-def t4rec_nvt_ds_path(t4rec_data_folder_path, locale, env, typ, cu_version: int, workflow_version: int):
+def t4rec_nvt_ds_path(t4rec_folder_path, locale, env, typ, cu_version: int, workflow_version: int):
     return os.path.join(
-        t4rec_data_folder_path, 'nvt_datasets', locale, env,
+        t4rec_folder_path, 'data', 'nvt_datasets', locale, env,
         '{}_cu_v{}_workflow_v{}'.format(typ, cu_version, workflow_version)
     )
 
 
-def t4rec_model_path(t4rec_models_folder_path, locale, env, model_name):
+def t4rec_model_path(t4rec_folder_path, locale, env, model_name):
     return os.path.join(
-        t4rec_models_folder_path, locale, env, model_name
+        t4rec_folder_path, 'models', 'transformers4rec', locale, env, model_name
     )
 
 
-def nvt_workflow_path(t4rec_data_folder_path, locale, env, workflow_version: int):
+def nvt_workflow_path(t4rec_folder_path, locale, env, workflow_version: int):
     return os.path.join(
-        t4rec_data_folder_path, 'nvt_workflows', locale, env,
+        t4rec_folder_path, 'data', 'transformers4rec', 'nvt_workflows', locale, env,
         'workflow_v{}'.format(workflow_version)
     )
 
