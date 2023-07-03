e30a5bfa6899df4171217a4eb58669e26d430fd2
Arsenii Vanunts <ar16sen@gmail.com>
2023-07-03 11:58:16 +0200

    Fix + change versions

--- src/transformers4RecKDD/t4rec/callbacks.py
+++ src/transformers4RecKDD/t4rec/callbacks.py
@@ -7,7 +7,7 @@ class CleanDriveTrashCheckpointsCallback(TrainerCallback):
     def __init__(self, drive):
         self.drive = drive
 
-    def on_save(self, args, state, control):
+    def on_save(self, args, state, control, **kwargs):
         max_num_checkpoints_in_trash = args.max_num_checkpoints_in_trash
         num_checkpoints_in_trash = get_num_checkpoints_in_trash(self.drive)
         if num_checkpoints_in_trash > max_num_checkpoints_in_trash:
